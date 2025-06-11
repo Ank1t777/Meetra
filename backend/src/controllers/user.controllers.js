436e106dbeef4c9858e7beb016a9b07b262277d2
Ankit_Kumar_Singh <63195545+Barman7085@users.noreply.github.com>
2025-06-12 00:00:24 +0530

    Added friendrequest checks final

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -1,3 +1,4 @@
+import FriendRequest from '../models/FriendRequest.js';
 import { User } from '../models/User.js';
 
 export async function getRecommendedUsers(req, res) {
@@ -28,6 +29,45 @@ export async function getMyFriends(req, res) {
         res.status(200).json(user.friends);
     } catch (error) {
         console.error("Error fetching friends:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export async function sendFriendRequest(req, res) {
+    try {
+        const myId = req.user.id;
+        const { id:recipientId } = req.params;
+
+        if(myId === recipientId) {
+            return res.status(400).json( { message: "You cannot send friend request to yourself" })
+        }
+
+        const recipient = await User.findById(recipientId);
+        if(!recipient) {
+            return res.status(404).json({ message: "Recipient not found" });
+        }
+        if(recipient.friends.includes(myId)) {
+            return res.status(400).json({ message: "You are already friends with this user" });
+        }
+
+        const existingRequest = await FriendRequest.findOne({
+            $or: [
+                { sender: myId, recipient: recipientId },
+                { sender: recipientId, recipient: myId }
+            ]
+        });
+
+        if(existingRequest) {
+            return res.status(400).json({ message: "Friend request already exists" });
+        }
+
+        const friendRequest = new FriendRequest({
+            sender: myId,
+            recipient: recipientId,
+        });
+        res.status(201).json(friendRequest);
+    } catch(error) {
+        console.error("Error sending friend request:", error);
         return res.status(500).json({ message: "Internal server error" });
     }
 }

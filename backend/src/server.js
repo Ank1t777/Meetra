da4fcefe3c69949943c0e1821b5aa0d80b8ed6a7
Ankit_Kumar_Singh <63195545+Barman7085@users.noreply.github.com>
2025-06-12 01:17:18 +0530

    Adding chat route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import "dotenv/config" ;
 import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+import chatRoutes from './routes/chat.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser'; // Import cookie-parser to handle cookies
 
@@ -12,6 +14,8 @@ app.use(express.json());
 app.use(cookieParser()); // Use cookie-parser middleware to parse cookies
 
 app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes); 
+app.use('/api/chat', chatRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

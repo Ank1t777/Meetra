22177214417af57ff815972a6a20a1ea9502db2e
Ankit_Kumar_Singh <63195545+Barman7085@users.noreply.github.com>
2025-06-13 00:12:23 +0530

    Finished setting up routes and frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
+import HomePage from './pages/HomePage.jsx';
+import SignupPage from './pages/SignupPage.jsx';
+import OnboardingPage from './pages/OnboardingPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import CallPage from './pages/CallPage.jsx';
+import ChatPage from './pages/ChatPage.jsx';
+import NotificationsPage from './pages/NotificationsPage.jsx';
+import toast, { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
     <div className='h-screen' data-theme="coffee">
+      <button onClick={() => toast.success('Welcome to Meetra')
+      }>Create a Toast</button>
       <Routes>
-        <Route path="/" element={<HomePage />} />}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login"element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
       </Routes>
+
+      <Toaster />
     </div>
   )
 }

264fa687894896251a261e77726cdbecd856a541
Ankit_Kumar_Singh <63195545+Barman7085@users.noreply.github.com>
2025-06-23 21:30:41 +0530

    Resolved the flag image issue

--- frontend/src/lib/getLanguageFlag.jsx
+++ frontend/src/lib/getLanguageFlag.jsx
@@ -7,13 +7,7 @@ export default function getLanguageFlag(language) {
   const countryCode = LANGUAGE_TO_FLAG[langLower];
 
   if (countryCode) {
-    return (
-      <img
-        src={`https://flagcdn.com/24x18/${countryCode}.png`}
-        alt={`${langLower} flag`}
-        className="h-3 mr-1 inline-block"
-      />
-    );
+    return `https://flagcdn.com/24x18/${countryCode}.png`;
   }
   return null;
 }

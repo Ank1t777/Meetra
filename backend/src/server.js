7dff21fb6f656d448823fef081590d8f8f06b321
Ankit_Kumar_Singh <63195545+Barman7085@users.noreply.github.com>
2025-06-25 02:56:14 +0530

    final touch

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,12 @@ import chatRoutes from './routes/chat.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser'; // Import cookie-parser to handle cookies
 import cors from 'cors'; // Import cors to handle CORS issues
+import path from 'path';
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: 'http://localhost:5174',
@@ -22,6 +24,13 @@ app.use(cookieParser()); // Use cookie-parser middleware to parse cookies
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes); 
 app.use('/api/chat', chatRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
